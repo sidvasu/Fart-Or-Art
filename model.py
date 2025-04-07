67f70f73af7acd530548d52546291088f8e9a038
Siddharth Vasu <siddharth.vasu.ca@gmail.com>
2025-04-06 19:05:38 -0700

    Added styling

--- model.py
+++ model.py
@@ -14,25 +14,26 @@ def extract_features(file_path, n_mfcc=13):
 
 # Load dataset
 def load_dataset(folder_pos, folder_neg):
-    X = []
-    y = []
+    X = [] # Positive class
+    y = [] # Negative class
     for file in os.listdir(folder_pos):
         if file.endswith('.wav'):
             features = extract_features(os.path.join(folder_pos, file))
             X.append(features)
-            y.append(1)  # Positive class
+            y.append(1) 
     for file in os.listdir(folder_neg):
         if file.endswith('.wav'):
             features = extract_features(os.path.join(folder_neg, file))
             X.append(features)
-            y.append(0)  # Negative class
+            y.append(0)
     return np.array(X), np.array(y)
 
+# Predicts audio classification
 def predict_audio_class(file_path):
     clf = joblib.load('audio_classifier.pkl')
 
     features = extract_features(file_path)
-    features = features.reshape(1, -1)  # Reshape for sklearn (1 sample, n_features)
+    features = features.reshape(1, -1)
     prediction = clf.predict(features)
     
     if prediction[0] == 1:
@@ -40,11 +41,9 @@ def predict_audio_class(file_path):
     else:
         return False
 
-# Example usage
 if __name__ == "__main__":
-    # Replace with your directories
-    pos_folder = "data/fart_dataset"  # e.g., "dog bark"
-    neg_folder = "data/music_dataset/Trumpet"  # e.g., "no bark"
+    pos_folder = "data/fart_dataset"
+    neg_folder = "data/music_dataset/Trumpet"
 
     X, y = load_dataset(pos_folder, neg_folder)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
